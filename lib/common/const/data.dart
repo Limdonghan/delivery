b8f16eb4f5b6454b763ec920c675d8f821d8962d
ImDongHan <dim95985@gmail.com>
2024-10-02 10:02:02 +0900

    Update data.dart

--- lib/common/const/data.dart
+++ lib/common/const/data.dart
@@ -5,7 +5,7 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 // ignore: constant_identifier_names
 const ACCESS_TOKEN_KEY = 'ACCESS_TOKEN';
 // ignore: constant_identifier_names
-const REFRESS_TOKEN_KEY = 'REFRESS_TOKEN';
+const REFRESH_TOKEN_KEY = 'REFRESS_TOKEN';
 
 const storage = FlutterSecureStorage();
 
